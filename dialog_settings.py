af0c14cf14126717089e54387c47f3d2523e8115
Your Name <detzold@gmail.com>
2014-06-17 07:59:38 +0200

    added option to select the preview generator to the settings

--- dialog_settings.py
+++ dialog_settings.py
@@ -11,7 +11,7 @@ class DialogSettings(gtk.Dialog):
 
     s = settings.Settings()
 
-    t = gtk.Table(rows = 2, columns = 3)
+    t = gtk.Table(rows = 3, columns = 3)
     t.set_col_spacings(10)
     t.set_row_spacings(10)
 
@@ -39,11 +39,29 @@ class DialogSettings(gtk.Dialog):
     l.show()
     self.pdf_location = l
     b = gtk.Button("Choose")
-    b.connect("clicked", self.choose_pdf_location, None)
-
-    t.attach(b, 2, 3, 1, 2)
     b.show()
+    b.connect("clicked", self.choose_pdf_location, None)
+    t.attach(b, 2, 3, 1, 2)
 
+
+    # ----
+    l = gtk.Label("Preview converter:")
+    l.set_alignment(xalign = 1.0, yalign = 0.5)
+    t.attach(l, 0, 1, 2, 3)
+    l.show()
+    l = gtk.Entry()
+    l.set_width_chars(40)
+    l.set_text(s.vars["pdfconvert"])
+    l.set_alignment(xalign = 0.0)
+    t.attach(l, 1, 2, 2, 3)
+    l.show()
+    self.pdf_convert = l
+    b = gtk.Button("Choose")
+    b.show()
+    b.connect("clicked", self.choose_pdf_convert, None)
+    t.attach(b, 2, 3, 2, 3)
+
+    # ----
     self.vbox.pack_start(t)
     t.show()
 
@@ -51,11 +69,11 @@ class DialogSettings(gtk.Dialog):
     self.add_button("Cancel", 2)
 
   def show(self):
-    r = self.run()
-    if r == 1:
+    if self.run() == 1:
       s = settings.Settings()
       s.vars["pdfviewer"] = self.pdf_viewer.get_text()
       s.vars["pdflocation"] = self.pdf_location.get_text()
+      s.vars["pdfconvert"] = self.pdf_convert.get_text()
       s.commit()
     self.destroy()
 
@@ -66,4 +84,13 @@ class DialogSettings(gtk.Dialog):
     r = f.run()
     if r == gtk.RESPONSE_OK:
       self.pdf_location.set_text(f.get_current_folder())
+    f.destroy()
+
+  def choose_pdf_convert(self, widget, data = None):
+    f = gtk.FileChooserDialog("Select an executable", self,
+      buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+    #f.set_action(gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER)
+    r = f.run()
+    if r == gtk.RESPONSE_OK:
+      self.pdf_convert.set_text(f.get_filename())
     f.destroy()
