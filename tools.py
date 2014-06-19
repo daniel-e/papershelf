b1405bb377176317d727c8e7d3b105fb48dcc4cb
Sascha Wittkowski <wittus@gmx.de>
2014-06-19 14:10:56 +0200

    bugfix

--- tools.py
+++ tools.py
@@ -41,7 +41,7 @@ def next_number():
 
 def download_pdf(url):
   f = tempfile.NamedTemporaryFile(delete = False, suffix = ".getpdf.pdf")
-  pdf = urllib2.urlopen(self.url.get_text()).read()
+  pdf = urllib2.urlopen(url).read()
   f.write(pdf)
   f.close()
   return f.name
